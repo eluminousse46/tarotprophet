2aec631c3bebcc3b8c0e741da0d121bb329ca21f
eluminousse46 <eluminous.se46@gmail.com>
2020-06-09 17:46:38 +0530

    first commit

---
+++ lp/life-force-tarot-imm/index.php
@@ -0,0 +1,96 @@
+<?php
+if(isset($_REQUEST['source']) && !empty($_REQUEST['source'])){
+	$source = $_REQUEST['source'];
+}
+if(isset($_REQUEST['vtid']) && !empty($_REQUEST['vtid'])){
+	$vtid = $_REQUEST['vtid'];
+}
+?>
+<!DOCTYPE html>
+<!--  This site was created in Webflow. http://www.webflow.com  -->
+<!--  Last Published: Thu May 28 2020 14:19:12 GMT+0000 (Coordinated Universal Time)  -->
+<html data-wf-page="5ecf65079d8fbe08e4df6445" data-wf-site="5ecf65079d8fbe632bdf6444">
+<head>
+  <meta charset="utf-8">
+  <title>Your Life Force Energy Tarot Reading</title>
+  <meta content="width=device-width, initial-scale=1" name="viewport">
+  <meta content="Webflow" name="generator">
+  <link href="css/normalize.css" rel="stylesheet" type="text/css">
+  <link href="css/webflow.css" rel="stylesheet" type="text/css">
+  <link href="css/your-life-force-energy-tarot-reading.webflow.css" rel="stylesheet" type="text/css">
+  <script src="https://ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js" type="text/javascript"></script>
+  <script type="text/javascript">WebFont.load({  google: {    families: ["Open Sans:300,300italic,400,400italic,600,600italic,700,700italic,800,800italic","PT Serif:400,400italic,700,700italic","Droid Serif:400,400italic,700,700italic"]  }});</script>
+  <!-- [if lt IE 9]><script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.min.js" type="text/javascript"></script><![endif] -->
+  <script type="text/javascript">!function(o,c){var n=c.documentElement,t=" w-mod-";n.className+=t+"js",("ontouchstart"in o||o.DocumentTouch&&c instanceof DocumentTouch)&&(n.className+=t+"touch")}(window,document);</script>
+  <link href="images/favicon.png" rel="shortcut icon" type="image/x-icon">
+  <link href="images/webclip.png" rel="apple-touch-icon">
+  <!-- Google Tag Manager Head -->
+  <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+  'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+  })(window,document,'script','dataLayer','GTM-5DM8D9');</script>
+  <!-- End Google Tag Manager -->
+</head>
+<body class="global">
+	<!-- Google Tag Manager (noscript) Body -->
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5DM8D9"
+    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
+  <div class="header-wrapper"><img src="images/tp-logo-white.png" alt="" class="logo"></div>
+  <div class="container">
+    <h1 class="header-h1">How To Wipe Out The Most Pressing Blockages That Are Preventing You From <span class="h1-highlight">Living The Life You Always Dreamed About</span></h1>
+    <h2 class="sub-header">By unlocking your ‘Life Force Energy’ through my free tarot card reading……</h2>
+    <div class="card-center"><a href="https://tarotprophet.com/tarotreadingsteps/lp/3-card-tarot/?source=<?php echo $source;?>&vtid=<?php echo $vtid;?>"><img src="images/card.png" alt="" class="card-image"></a></div>
+    <div class="center-div mobile">
+      <a href="https://tarotprophet.com/tarotreadingsteps/lp/3-card-tarot/?source=<?php echo $source;?>&vtid=<?php echo $vtid;?>" class="button w-inline-block">
+        <div class="arrow"></div>
+        <div>Start My &#x27;<span class="button-highlight">Life Force Energy</span>&#x27; Tarot Reading Now</div>
+      </a>
+    </div>
+    <div class="important-wrapper">
+      <div><strong class="important">IMPORTANT: </strong></div>
+      <div>I can only deliver a limited number of tarot readings each day, and <strong>this special offer will be removed</strong> once I have too many requests.</div>
+    </div>
+    <div class="paragraph">So <strong>request your FREE ‘Life Force Energy’ tarot reading</strong> while it is still available…</div>
+    <div class="about-wrapper">
+      <div class="center-div">
+        <a href="https://tarotprophet.com/tarotreadingsteps/lp/3-card-tarot/?source=<?php echo $source;?>&vtid=<?php echo $vtid;?>" class="button w-inline-block">
+          <div class="arrow"></div>
+          <div>Start My &#x27;<span class="button-highlight">Life Force Energy</span>&#x27; Tarot Reading Now</div>
+        </a>
+      </div>
+      <div class="flex-display center-mid">
+        <div class="about-picture-circle-wrapper"><img src="images/author.jpg" alt="" class="about-picture"></div>
+        <div class="about-name">Celebrated Psychic<br>Tarot Prophet</div>
+      </div>
+    </div>
+    <div>
+      <div class="sepator-wrapper"><img src="images/separator.png" alt=""></div>
+      <div>
+        <div class="testimonial">&quot;Sophia cleared indecisiveness, pinpointed root causes of my setbacks and challenges and then gave me another approach to tackle my issues. <br>‍<br>I do recommend Tarot Prophet to anyone who wants to know about themselves and those who want spiritual advice.&quot;<br>‍<br> - <span class="testimonial-name">Ezekiel</span></div>
+      </div>
+      <div class="sepator-wrapper"><img src="images/separator.png" alt=""></div>
+      <div class="fb-comment-wrapper"><img src="images/fb-comments-p-500.png" alt="" class="fb-comment"></div>
+    </div>
+    <div class="center-div">
+      <a href="https://tarotprophet.com/tarotreadingsteps/lp/3-card-tarot/?source=<?php echo $source;?>&vtid=<?php echo $vtid;?>" class="button w-inline-block">
+        <div class="arrow"></div>
+        <div>Start My &#x27;<span class="button-highlight">Life Force Energy</span>&#x27; Tarot Reading Now</div>
+      </a>
+    </div>
+  </div>
+  <div class="footer-wrapper">
+    <div class="footer">
+      <p>*DISCLAIMER: Your email address will be used to send you your Free Tarot Reading and ongoing information regarding your Tarot Reading and related promotions. You may unsubscribe from receiving emails at any time with a single click on the Unsubscribe link included in all emails. At the end of your free Tarot Reading, you will have the option to purchase an in-depth Tarot Reading at only $39. This is completely optional.<br>‍<br>Images used for testimonials are a representation of the customers only and not their actual photos. Some names and identifying details have been changed to protect the privacy of individuals.<br>‍<br>The information provided on TarotProphet.com, and in any reports, emails, ebooks, or other materials you might receive from TarotProphet.com, is for entertainment purposes only. Obviously, we cannot guarantee the accuracy of TarotProphet.com&#x27;s information, and we are not responsible for how you interpret or apply it. If you are struggling with serious problems, including chronic illness, mental instability, or legal issues, please consult with your local registered health care or legal professional as soon as possible. In other words, please use common sense. Please also carefully read the full &quot;<a href="https://tarotprophet.com/terms-and-conditions/" target="_parent" class="link">Terms &amp; Conditions</a>&quot;.</p>
+      <div>
+        <div><a href="https://tarotprophet.com/terms-and-conditions/" target="_parent" class="link margin">Terms &amp; Conditions</a> <a href="https://tarotprophet.com/privacy-policy/" target="_parent" class="link margin">Privacy Policy</a> <a href="https://tarotprophet.com/contact/" target="_parent" class="link margin">Contact Us</a></div>
+      </div>
+    </div>
+  </div>
+  <div class="global-background"></div>
+  <script src="https://d3e54v103j8qbb.cloudfront.net/js/jquery-3.4.1.min.220afd743d.js?site=5ecf65079d8fbe632bdf6444" type="text/javascript" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
+  <script src="js/webflow.js" type="text/javascript"></script>
+  <!-- [if lte IE 9]><script src="https://cdnjs.cloudflare.com/ajax/libs/placeholders/3.0.2/placeholders.min.js"></script><![endif] -->
+</body>
+</html>
